fa6995a9dbd4cd8aaaa0335717c58449fa28518c
dewi-ny-je <2866139+dewi-ny-je@users.noreply.github.com>
2026-04-22 10:46:37 +0200

    Refactor to use config for state sensor retrieval

--- custom_components/solar_savings/__init__.py
+++ custom_components/solar_savings/__init__.py
@@ -47,10 +47,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass, STORAGE_VERSION, f"{STORAGE_KEY}.{entry.entry_id}"
     )
     calculator = SolarSavingsCalculator.from_dict(await store.async_load())
+    config = {**entry.data, **entry.options}
 
-    solar_state = hass.states.get(entry.data[CONF_SOLAR_ENERGY_SENSOR])
-    import_state = hass.states.get(entry.data[CONF_IMPORT_ENERGY_SENSOR])
-    export_state = hass.states.get(entry.data[CONF_EXPORT_ENERGY_SENSOR])
+    solar_state = hass.states.get(config[CONF_SOLAR_ENERGY_SENSOR])
+    import_state = hass.states.get(config[CONF_IMPORT_ENERGY_SENSOR])
+    export_state = hass.states.get(config[CONF_EXPORT_ENERGY_SENSOR])
     calculator.seed(
         solar_energy=to_decimal(solar_state.state if solar_state else None),
         import_energy=to_decimal(import_state.state if import_state else None),
@@ -65,9 +66,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         async_dispatcher_send(hass, f"{SIGNAL_UPDATED}_{entry.entry_id}")
 
     def handle_grid_event(event: Event) -> None:
-        import_state = hass.states.get(entry.data[CONF_IMPORT_ENERGY_SENSOR])
-        export_state = hass.states.get(entry.data[CONF_EXPORT_ENERGY_SENSOR])
-        price_state = hass.states.get(entry.data[CONF_EXPORT_PRICE_SENSOR])
+        import_state = hass.states.get(config[CONF_IMPORT_ENERGY_SENSOR])
+        export_state = hass.states.get(config[CONF_EXPORT_ENERGY_SENSOR])
+        price_state = hass.states.get(config[CONF_EXPORT_PRICE_SENSOR])
         changed = calculator.handle_grid_update(
             import_energy=to_decimal(import_state.state if import_state else None),
             export_energy=to_decimal(export_state.state if export_state else None),
@@ -77,8 +78,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             hass.async_create_task(async_save_and_update())
 
     def handle_solar_event(event: Event) -> None:
-        solar_state = hass.states.get(entry.data[CONF_SOLAR_ENERGY_SENSOR])
-        price_state = hass.states.get(entry.data[CONF_IMPORT_PRICE_SENSOR])
+        solar_state = hass.states.get(config[CONF_SOLAR_ENERGY_SENSOR])
+        price_state = hass.states.get(config[CONF_IMPORT_PRICE_SENSOR])
         changed = calculator.handle_solar_update(
             solar_energy=to_decimal(solar_state.state if solar_state else None),
             import_price=to_decimal(price_state.state if price_state else None),
@@ -91,22 +92,28 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             async_track_state_change_event(
                 hass,
                 [
-                    entry.data[CONF_IMPORT_ENERGY_SENSOR],
-                    entry.data[CONF_EXPORT_ENERGY_SENSOR],
+                    config[CONF_IMPORT_ENERGY_SENSOR],
+                    config[CONF_EXPORT_ENERGY_SENSOR],
                 ],
                 handle_grid_event,
             ),
             async_track_state_change_event(
                 hass,
-                [entry.data[CONF_SOLAR_ENERGY_SENSOR]],
+                [config[CONF_SOLAR_ENERGY_SENSOR]],
                 handle_solar_event,
             ),
         ]
     )
 
     await hass.config_entries.async_forward_entry_setups(entry, platforms)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     await store.async_save(calculator.as_dict())
     return True
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload Solar Savings when options are changed."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
